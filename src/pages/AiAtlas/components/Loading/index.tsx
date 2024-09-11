3b0ca2124f63e92b46a8ee85d46873829dc426bf
jyying <jinyuy@seentao.com>
2024-09-11 13:26:28 +0800

    feat: ai智能图谱

--- src/pages/AiAtlas/components/Loading/index.tsx
+++ src/pages/AiAtlas/components/Loading/index.tsx
@@ -1,12 +1,12 @@
 import loadingPng from '@/assets/images/aiAtlas/loading.png';
 import React from 'react';
-import styles from './index.less';
+import styles from '../Empty/index.less';
 
 type Props = { loadingMessage: string };
 const Loading: React.FC<Props> = ({ loadingMessage }) => {
   return (
     <div className={styles['container']}>
-      <img src={loadingPng} />
+      <img src={loadingPng} className={styles['rotate']} />
       <span>{loadingMessage}</span>
     </div>
   );
--- src/pages/AiAtlas/index.tsx
+++ src/pages/AiAtlas/index.tsx
@@ -1,26 +1,192 @@
 // ai图谱
-import React from 'react';
+import PageLoading from '@/components/PageLoading';
+import {
+  extractKnowledge,
+  generateAtlas,
+  getAtlasHistory,
+  getRandomCase,
+} from '@/services/aiAtlas';
+import { getConvertParamId } from '@/services/aiJobHunt';
+import { useMount, useReactive } from 'ahooks';
+import { useRef } from 'react';
+import KnowledgeGraph from './KnowledgeGraph';
+import KnowledgeMsg from './KnowledgeMsg';
+import TextContent from './TextContent';
+import Empty from './components/Empty';
 import Loading from './components/Loading';
+import { CREATE_STATUS, EXTRACT_STATUS, PluginCode } from './constants';
 import styles from './index.less';
+import { AtlasInfo, GraphDataType, TableDataType } from './type';
 
-type Props = {};
-const AiAtlas: React.FC<Props> = ({}) => {
+type IState = {
+  data: null | AtlasInfo;
+  loading: boolean;
+  extractLoading: boolean;
+  graphLoading: boolean;
+  graphData: GraphDataType | null;
+};
+
+const AiAtlas = ({}) => {
+  const state = useReactive<IState>({
+    data: null,
+    loading: true,
+    extractLoading: false,
+    graphLoading: false,
+    graphData: null,
+  });
+
+  const randomId = useRef<string>('');
+
+  useMount(() => {
+    const queryParams = JSON.parse(
+      // @ts-ignore
+      window.sessionStorage.getItem('queryParams'),
+    );
+    getConvertParamId(queryParams)
+      .then((id) => {
+        window.sessionStorage.setItem(
+          'queryParams',
+          JSON.stringify({ ...queryParams, paramId: id }),
+        );
+        getAtlasHistory().then((rst) => {
+          if (rst) {
+            state.data = rst;
+            generateGraph(JSON.parse(rst.info));
+          }
+        });
+        state.loading = false;
+      })
+      .catch(() => {
+        state.loading = false;
+      });
+  });
+
+  // 抽取知识
+  const extractMsg = (userMessage: string) => {
+    state.extractLoading = true;
+    state.data!.extractState = EXTRACT_STATUS.SUCCESS;
+    state.data!.createState = CREATE_STATUS.UNGENERATED;
+    if (userMessage) {
+      extractKnowledge({ userMessage, pluginCode: PluginCode })
+        .then((rst) => {
+          if (rst) {
+            state.data!.info = rst.info;
+          }
+          state.extractLoading = false;
+        })
+        .catch(() => {
+          state.extractLoading = false;
+        });
+    }
+  };
+
+  // 生成图谱
+  const generateGraph = (graphData: TableDataType[]) => {
+    const edges: GraphDataType['edges'] = [];
+    const nodes: GraphDataType['nodes'] = [];
+    graphData.forEach((item) => {
+      let sourceNode = nodes.find((node) => node.label === item.entity1);
+      if (!sourceNode) {
+        sourceNode = {
+          id: item.entity1,
+          label: item.entity1,
+          level: 0,
+        };
+        nodes.push(sourceNode);
+      }
+      let targetNode = nodes.find((node) => node.label === item.entity2);
+      if (!targetNode) {
+        nodes.push({
+          id: item.entity2,
+          label: item.entity2,
+          level: sourceNode.level + 1,
+        });
+      }
+      edges.push({
+        source: item.entity1,
+        target: item.entity2,
+        label: item.rel,
+      });
+    });
+    state.graphData = {
+      nodes,
+      edges,
+    };
+    setTimeout(() => {
+      state.graphLoading = false;
+      state.data!.createState = CREATE_STATUS.GENERATED;
+    }, 1000);
+  };
+
+  // 随机示例
+  const createRandomCase = () => {
+    getRandomCase({ id: randomId.current, pluginCode: PluginCode }).then(
+      (rst) => {
+        randomId.current = rst.id;
+        state.data = rst;
+        generateGraph(JSON.parse(state.data!.info));
+      },
+    );
+  };
+
   return (
     <div className={styles['container']}>
       <header>AI图谱生成</header>
-      <section>
-        <div className={styles['area-content']}>
-          <div className={styles['area-text-container']}>
-            <div></div>
+      {state.loading ? (
+        <PageLoading />
+      ) : (
+        <section>
+          <div className={styles['area-content']}>
+            <div className={styles['area-text-body']}>
+              <div className={styles['area-text']}>
+                <TextContent
+                  isFirst={!state.data?.content}
+                  content={state.data?.content || ''}
+                  createRandomCase={createRandomCase}
+                  extractMsg={extractMsg}
+                  extractLoading={state.extractLoading}
+                />
+              </div>
+              <div className={styles['area-knowledge']}>
+                <KnowledgeMsg
+                  extractLoading={state.extractLoading}
+                  knowledgeInfo={
+                    state.data?.extractState === EXTRACT_STATUS.SUCCESS
+                      ? state.data?.info || ''
+                      : ''
+                  }
+                  generateGraph={(data) => {
+                    state.graphLoading = true;
+                    generateAtlas({
+                      id: state.data?.id,
+                      info: JSON.stringify(data),
+                    })
+                      .then(() => {
+                        generateGraph(data);
+                      })
+                      .catch(() => {
+                        state.graphLoading = false;
+                      });
+                  }}
+                  graphLoading={state.graphLoading}
+                ></KnowledgeMsg>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className={styles['area-atlas-container']}>
-          <header>知识图谱</header>
-          <section>
-            <Loading loadingMessage="待生成知识图谱" />
-          </section>
-        </div>
-      </section>
+          <div className={styles['area-atlas-container']} id="graphContent">
+            <header>知识图谱</header>
+            <section>
+              {state.graphLoading ? (
+                <Loading loadingMessage="知识图谱生成中" />
+              ) : state.data?.createState === CREATE_STATUS.GENERATED ? (
+                <KnowledgeGraph data={state.graphData} />
+              ) : (
+                <Empty emptyMessage="待生成知识图谱" />
+              )}
+            </section>
+          </div>
+        </section>
+      )}
     </div>
   );
 };
