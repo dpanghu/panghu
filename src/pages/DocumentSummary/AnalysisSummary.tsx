0c0011c5e6c7287c76f061e9758d4a956f804959
zhangkejing <zhangkejing@seentao.com>
2024-10-09 10:55:00 +0800

    fix: 文档总结思维导图优化

--- src/pages/DocumentSummary/AnalysisSummary.tsx
+++ src/pages/DocumentSummary/AnalysisSummary.tsx
@@ -1,6 +1,7 @@
 import FilePreview from '@/components/FilePreview';
 import TreeGraph from '@/components/TreeGraph';
 import { useReactive } from 'ahooks';
+import { nanoid } from 'nanoid';
 import React, { useEffect } from 'react';
 import AnalysisResult from './AnalysisResult';
 import styles from './AnalysisSummary.less';
@@ -24,21 +25,49 @@ const AnalysisSummary: React.FC<TProps> = ({
     isExitFullscreen: false,
   });
 
+  const addProperties = (data: any[], level = 2) => {
+    data.forEach((item: any, index) => {
+      item.level = level;
+      item.id = nanoid();
+      item.originId = nanoid();
+      item.order = 1 + index;
+      if (item.children && item.children.length > 0) {
+        addProperties(item.children, level + 1);
+      }
+    });
+  };
+
   const formatData = () => {
+    const match = summaryData?.attachmentName?.replace(/\.[^.]*$/, '');
     const data = JSON.parse(summaryData?.mindMap || '[]');
     if (data?.length && data?.length === 1) {
-      state.mindData = data[0];
+      state.mindData = {
+        ...data[0],
+        id: nanoid(),
+        originId: nanoid(),
+        order: 1,
+        level: 1,
+      };
     } else if (data?.length && data?.length > 1) {
       state.mindData = {
-        name: '文档总结',
+        name: match,
         children: data,
+        id: nanoid(),
+        originId: nanoid(),
+        order: 1,
+        level: 1,
       };
     } else {
       state.mindData = {
-        name: '文档总结',
+        name: match,
         children: [],
+        id: nanoid(),
+        originId: nanoid(),
+        order: 1,
+        level: 1,
       };
     }
+    addProperties(state.mindData.children);
   };
 
   const handleFullscreen = (visible: boolean) => {
